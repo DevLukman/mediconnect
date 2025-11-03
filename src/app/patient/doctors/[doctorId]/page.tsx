cf24ea05f831973aa5457133f1eb894a6e131e32
Lukas <MuhammedLukman2001@gmail.com>
2025-11-03 13:23:21 +0100

    clean up related to patient/doctors

--- src/app/patient/doctors/[doctorId]/page.tsx
+++ src/app/patient/doctors/[doctorId]/page.tsx
@@ -2,6 +2,5 @@ export default async function DoctorDetailPage(
   props: PageProps<"/patient/doctors/[slug]">,
 ) {
   const { slug } = await props.params;
-  console.log(slug);
   return <h1>Hello, doctor with id of {slug}</h1>;
 }
